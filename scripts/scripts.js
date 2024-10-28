2307c2a4378395880c882e8d108ff04bd5da4476
pushparajanramar <109318152+pushparajanramar@users.noreply.github.com>
2024-10-28 11:30:06 -0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -71,8 +71,8 @@ function decorateLinks(main) {
             link.setAttribute('href', convertToRelative(href));
         }
         // Add an id attribute based on the relative URL
-        const idValue = relativeHref.replace(/[^\w-]+/g, '_'); // Create a valid id by replacing invalid characters
-        link.setAttribute('id', `link-${idValue}`);
+        // const idValue = relativeHref.replace(/[^\w-]+/g, '_'); // Create a valid id by replacing invalid characters
+        // link.setAttribute('id', `link-${idValue}`);
 
     });
 }
